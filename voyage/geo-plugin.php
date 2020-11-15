70473991905498d22232d9033270a9c552dc7a18
adicore <56226709+adicore@users.noreply.github.com>
2020-11-15 09:04:12 +0700

    Update and rename geoplugin.php to geo-plugin.php

--- voyage/geo-plugin.php
+++ voyage/geo-plugin.php
@@ -1,4 +1,3 @@
 $data = unserialize(file_get_contents('http://www.geoplugin.net/php.gpip='.$_SERVER['REMOTE_ADDR'])));
 //Then access the data directly.
 echo $data['geoplugin_city'];
-echo $data['geoplugin_region'];
