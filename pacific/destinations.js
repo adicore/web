ca7473d5b5dcf30e9d1210e9faef6db6a8b5709c
adicore <56226709+adicore@users.noreply.github.com>
2021-04-25 01:05:12 +0700

    Update destinations.js

--- pacific/destinations.js
+++ pacific/destinations.js
@@ -1,6 +1,6 @@
 $.getJSON("https://suggest.travelpayouts.com/weedle?destination_iata=JKT&amp;locale=en&amp;currency=usd&amp;limit=10", function(result){
        console.log(result);
        $.each(result, function(i){
-         document.getElementById("flightx").innerHTML +=" "+ result[i]destination.city_name + ", " + result[i]origin.city_name + " ";
+         document.getElementById("flightx").innerHTML +=" "+ result[i].destination.city_name + ", " + result[i].origin.city_name + " ";
        });
      });
