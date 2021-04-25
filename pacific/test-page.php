fdc982782b76ddad7a56092ad75e70e5954ac326
adicore <56226709+adicore@users.noreply.github.com>
2021-04-26 04:21:52 +0700

    Create test-page.php

---
+++ pacific/test-page.php
@@ -0,0 +1,16 @@
+<section class='ftco-section img ftco-select-destination lazyload lazy lazy-background' loading='lazy' style='background-image:url(https://cdn.traveltime.xyz/pacific/img/image_bg3.webp)'><div class='container'><div class='row justify-content-center pb-4'><div class='col-md-12 heading-section text-center ftco-animate'><span class='subheading'>Pacific Provide Places</span><h2 class='mb-4'>Select Your Destination</h2></div></div></div><div class='container'><div class='row'><div class='col-md-12'><b:section class='carousel-destination owl-carousel ftco-animate' id='json' maxwidgets='1' name='Test' showaddelement='no'>
+  <b:widget id='Image5' locked='true' title='Json' type='Image' version='2' visible='true'>
+    <b:widget-settings>
+      <b:widget-setting name='displayUrl'>https://cdn.traveltime.xyz/pacific/img/place-1.webp</b:widget-setting>
+      <b:widget-setting name='displayHeight'>242</b:widget-setting>
+      <b:widget-setting name='sectionWidth'>1110</b:widget-setting>
+      <b:widget-setting name='shrinkToFit'>false</b:widget-setting>
+      <b:widget-setting name='displayWidth'>1110</b:widget-setting>
+      <b:widget-setting name='link'>/</b:widget-setting>
+      <b:widget-setting name='caption'/>
+    </b:widget-settings>
+    <b:includable id='main'><div class='item'><div class='project-destination'><a class='img lazyload lazy lazy-background' expr:alt='data:title' expr:height='data:height' expr:href='data:link' expr:id='data:widget.instanceId + &quot;_img&quot;' expr:style='&quot;background-image: url(\&quot;&quot; + data:sourceUrl + &quot;\&quot;); &quot;' expr:title='data:title' expr:width='data:width' loading='lazy'><div class='text'><h3><p id='flightx'/></h3><span><data:caption/></span></div></a></div></div><script src='https://cdn.traveltime.xyz/pacific/destinations.js'/></b:includable>
+    <b:includable id='content'><div class='widget-content'><b:tag cond='data:link' expr:href='data:link' name='a'><img expr:alt='data:title' expr:height='data:height' expr:id='data:widget.instanceId + &quot;_img&quot;' expr:src='data:sourceUrl' expr:width='data:width'><b:attr cond='data:sourceSet' expr:value='data:sourceSet' name='srcset'/></img></b:tag><br/><b:if cond='data:caption'><span class='caption'><data:caption/></span></b:if></div></b:includable>
+    <b:includable id='flightx'><span id='flightx'/></b:includable>
+  </b:widget>
+</b:section></div></div></div></section>
